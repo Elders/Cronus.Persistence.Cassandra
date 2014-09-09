5425b95c50c39c5de6881165b0340b41d281d544
nikolay.mankov <nikolay.mankov@mentormate.com>
2014-09-10 02:30:50 +0300

    Moved from Cronus repo

---
+++ src/Elders.Cronus.Sample.Collaboration.Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using Elders.Cronus.DomainModeling;
+
+[assembly: AssemblyTitle("Elders.Cronus.Sample.Collaboration.Contracts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: BoundedContext("Elders.Cronus.Sample.Collaboration")]
---
+++ src/Elders.Cronus.Sample.Collaboration.Handlers/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using Elders.Cronus.DomainModeling;
+
+[assembly: AssemblyTitle("Elders.Cronus.Sample.Ports")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: BoundedContext("Elders.Cronus.Sample.Collaboration")]

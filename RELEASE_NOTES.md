4b07d0d2cc97c793cfcdb9d92d6c9334fba67364
mynkow <mynkow@gmail.com>
2014-09-11 12:35:52 +0300

    Bump version to 1.0.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 1.0.1 - 11.09.2014
+* Fix bug with nuget package release
+
 #### 1.0.0 - 10.09.2014
 * Moved from the Cronus repository
--- src/Elders.Cronus.Persistence.Cassandra/Properties/AssemblyInfo.cs
+++ src/Elders.Cronus.Persistence.Cassandra/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("Elders.Cronus.Persistence.Cassandra")]
 [assembly: AssemblyDescriptionAttribute("Elders.Cronus.Persistence.Cassandra")]
 [assembly: AssemblyProductAttribute("Elders.Cronus.Persistence.Cassandra")]
-[assembly: AssemblyVersionAttribute("1.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0")]
+[assembly: AssemblyVersionAttribute("1.0.1")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: AssemblyFileVersionAttribute("1.0.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.0";
+        internal const string Version = "1.0.1";
     }
 }

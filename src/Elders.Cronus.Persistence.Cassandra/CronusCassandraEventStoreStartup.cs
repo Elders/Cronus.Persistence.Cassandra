662adb1144d6c884dee90f6b73649d18747023d0
mynkow <mynkow@gmail.com>
2018-12-12 13:58:54 +0200

    Adds CronusCassandraEventStoreStartup

---
+++ src/Elders.Cronus.Persistence.Cassandra/CronusCassandraEventStoreStartup.cs
@@ -0,0 +1,33 @@
+﻿using Elders.Cronus.Hosting;
+using Elders.Cronus.MessageProcessing;
+using Elders.Cronus.Multitenancy;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Elders.Cronus.Persistence.Cassandra
+{
+    public class CronusCassandraEventStoreStartup : ICronusStartup
+    {
+        private readonly IServiceProvider serviceProvider;
+        private readonly ITenantList tenants;
+
+        public CronusCassandraEventStoreStartup(IServiceProvider serviceProvider, ITenantList tenants)
+        {
+            this.serviceProvider = serviceProvider;
+            this.tenants = tenants;
+        }
+
+        public void Bootstrap()
+        {
+            foreach (var tenant in tenants.GetTenants())
+            {
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    CronusContext context = scope.ServiceProvider.GetRequiredService<CronusContext>();
+                    context.Initialize(tenant, scope.ServiceProvider);
+                    scope.ServiceProvider.GetRequiredService<CassandraEventStoreSchema>().CreateStorage();
+                }
+            }
+        }
+    }
+}

7fe7d751dacbc4896763208fcca9fdf950d9d3e2
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-09 23:18:37 +0300

    Call the async methods when using synchronous aggregate loading

--- src/Elders.Cronus.Persistence.Cassandra/CassandraEventStore.cs
+++ src/Elders.Cronus.Persistence.Cassandra/CassandraEventStore.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Cassandra;
 using Elders.Cronus.EventStore;
 using Elders.Cronus.Persistence.Cassandra.Logging;
@@ -130,27 +129,7 @@ namespace Elders.Cronus.Persistence.Cassandra
 
         public IEnumerable<AggregateCommit> LoadAggregateCommits(int batchSize)
         {
-            var queryStatement = GetReplayStatement().Bind().SetPageSize(batchSize);
-            RowSet result = session.Execute(queryStatement);
-            foreach (var row in result.GetRows())
-            {
-                var data = row.GetValue<byte[]>("data");
-                using (var stream = new MemoryStream(data))
-                {
-                    AggregateCommit commit;
-                    try
-                    {
-                        commit = (AggregateCommit)serializer.Deserialize(stream);
-                    }
-                    catch (Exception ex)
-                    {
-                        string error = "[EventStore] Failed to deserialize an AggregateCommit. EventBase64bytes: " + Convert.ToBase64String(data);
-                        log.ErrorException(error, ex);
-                        continue;
-                    }
-                    yield return commit;
-                }
-            }
+            return LoadAggregateCommitsAsync().Wait().Result;
         }
 
         public async IAsyncEnumerable<AggregateCommit> LoadAggregateCommitsAsync()
@@ -181,22 +160,7 @@ namespace Elders.Cronus.Persistence.Cassandra
 
         public IEnumerable<AggregateCommitRaw> LoadAggregateCommitsRaw(int batchSize = 5000)
         {
-            var queryStatement = GetReplayStatement().Bind().SetPageSize(batchSize);
-            var result = session.Execute(queryStatement);
-            foreach (var row in result.GetRows())
-            {
-                string id = row.GetValue<string>("id");
-                byte[] data = row.GetValue<byte[]>("data");
-                int revision = row.GetValue<int>("rev");
-                long timestamp = row.GetValue<long>("ts");
-
-                using (var stream = new MemoryStream(data))
-                {
-                    AggregateCommitRaw commitRaw = new AggregateCommitRaw(id, data, revision, timestamp);
-
-                    yield return commitRaw;
-                }
-            }
+            return LoadAggregateCommitsRawAsync().Wait().Result;
         }
 
         public async IAsyncEnumerable<AggregateCommitRaw> LoadAggregateCommitsRawAsync()

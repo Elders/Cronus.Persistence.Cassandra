b36ae96dcbde3762de3078e6db50b3ac2c4ae570
mynkow <mynkow@gmail.com>
2020-10-01 10:25:06 +0300

    Adds pagination when enumerating indices

--- src/Elders.Cronus.Persistence.Cassandra/CassandraEventStoreDiscovery.cs
+++ src/Elders.Cronus.Persistence.Cassandra/CassandraEventStoreDiscovery.cs
@@ -49,7 +49,6 @@ namespace Elders.Cronus.Persistence.Cassandra
             yield return new DiscoveredModel(typeof(CassandraReplicationStrategyFactory), typeof(CassandraReplicationStrategyFactory), ServiceLifetime.Singleton);
             yield return new DiscoveredModel(typeof(ICassandraReplicationStrategy), provider => provider.GetRequiredService<CassandraReplicationStrategyFactory>().GetReplicationStrategy(), ServiceLifetime.Transient);
 
-            yield return new DiscoveredModel(typeof(IIndexStatusStore), typeof(CassandraIndexStatusStore), ServiceLifetime.Transient) { CanOverrideDefaults = true };
             yield return new DiscoveredModel(typeof(IIndexStore), typeof(IndexByEventTypeStore), ServiceLifetime.Transient) { CanOverrideDefaults = true };
 
             yield return new DiscoveredModel(typeof(ICassandraEventStoreSchema), typeof(CassandraEventStoreSchema), ServiceLifetime.Transient);

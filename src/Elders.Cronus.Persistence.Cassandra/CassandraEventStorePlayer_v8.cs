cdb44222980bd0292eee5ab28dac8ca1800df191
Ivan Gyoshev <ivan.gyoshev@eldersoss.com>
2023-10-06 14:02:52 +0300

    fix: Handles 'null' case when deserializing AggregateCommit in CassandraEventStorePlayer_v8.LoadAggregateCommitsAsync overload

--- src/Elders.Cronus.Persistence.Cassandra/CassandraEventStorePlayer_v8.cs
+++ src/Elders.Cronus.Persistence.Cassandra/CassandraEventStorePlayer_v8.cs
@@ -103,8 +103,20 @@ namespace Elders.Cronus.Persistence.Cassandra
             {
                 var data = row.GetValue<byte[]>("data");
                 AggregateCommit commit = serializer.DeserializeFromBytes<AggregateCommit>(data);
+                if (commit is null)
+                {
+                    using (logger.BeginScope(s => s
+                                                    .AddScope(Log.AggregateId, row.GetValue<string>("id"))
+                                                    .AddScope("cronus_arrev", row.GetValue<int>("rev"))))
+                    {
+                        logger.Warn(() => "Unable to load aggregate commit and it will be skipped.");
 
-                yield return commit;
+                    }
+                }
+                else
+                {
+                    yield return commit;
+                }
             }
         }
 

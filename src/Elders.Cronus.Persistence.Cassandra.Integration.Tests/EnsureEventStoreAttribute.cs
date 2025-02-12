791ed07e0295a0fbd013c8a20f38a9cc34d309aa
Georgi Shukov <georgi.shukov98@gmail.com>
2025-02-12 15:56:43 +0200

    fix: Updates xunit.v3 to stable version

--- src/Elders.Cronus.Persistence.Cassandra.Integration.Tests/EnsureEventStoreAttribute.cs
+++ src/Elders.Cronus.Persistence.Cassandra.Integration.Tests/EnsureEventStoreAttribute.cs
@@ -6,9 +6,15 @@ namespace Elders.Cronus.Persistence.Cassandra.Integration.Tests;
 [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
 public class EnsureEventStoreAttribute : BeforeAfterTestAttribute
 {
-    public override async ValueTask Before(MethodInfo methodUnderTest, IXunitTest test)
+    /// <summary>
+    /// In xunit.v3 this method is called before the test method is called. In version 0.3-pre it was async,
+    /// but in the stable version it is not. If we didn't wait to finish the schema creation, the test will start before the schema is created which causes issues.
+    /// </summary>
+    /// <param name="methodUnderTest"></param>
+    /// <param name="test"></param>
+    public override void Before(MethodInfo methodUnderTest, IXunitTest test)
     {
         var schemaFixture = new CassandraEventStoreSchemaFixture(CassandraFixture.Instance);
-        await schemaFixture.GetEventStoreSchema(new NoTableNamingStrategy()).CreateStorageAsync();
+        schemaFixture.GetEventStoreSchema(new NoTableNamingStrategy()).CreateStorageAsync().GetAwaiter().GetResult();
     }
 }

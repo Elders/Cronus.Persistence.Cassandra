fc70eaad7c4136b120d3f3d85d498b5d14f3f0ef
Georgi Shukov <georgi.shukov98@gmail.com>
2025-03-31 17:26:52 +0300

    Implements tenant data wipe

---
+++ src/Elders.Cronus.Persistence.Cassandra/TenantDataWiper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cassandra;
+using Elders.Cronus.DangerZone;
+using Elders.Cronus.MessageProcessing;
+using Microsoft.Extensions.Logging;
+
+namespace Elders.Cronus.Persistence.Cassandra;
+
+public class TenantDataWiper : IDangerZone
+{
+    private readonly ICassandraProvider cassandraProvider;
+    private readonly ICronusContextAccessor cronusContextAccessor;
+    private readonly ILogger<TenantDataWiper> logger;
+
+    private DropKeyspaceQuery _dropKeyspaceQuery;
+
+    private Task<ISession> GetSessionAsync() => cassandraProvider.GetSessionAsync(); // In order to keep only 1 session alive (https://docs.datastax.com/en/developer/csharp-driver/3.16/faq/)
+
+    public TenantDataWiper(ICronusContextAccessor cronusContextAccessor, ICassandraProvider cassandraProvider, ILogger<TenantDataWiper> logger)
+    {
+        if (cassandraProvider is null) throw new ArgumentNullException(nameof(cassandraProvider));
+
+        this.cassandraProvider = cassandraProvider;
+        this.cronusContextAccessor = cronusContextAccessor;
+        this.logger = logger;
+
+        _dropKeyspaceQuery = new DropKeyspaceQuery(cronusContextAccessor, cassandraProvider);
+    }
+
+    public async Task WipeDataAsync(string tenant)
+    {
+        try
+        {
+            if (tenant.Equals(cronusContextAccessor.CronusContext.Tenant, StringComparison.Ordinal) == false)
+            {
+                logger.LogError("Tenant mismatch. The tenant to be wiped is different from the current tenant.");
+                return;
+            }
+
+            ISession session = await GetSessionAsync().ConfigureAwait(false);
+            PreparedStatement statement = await _dropKeyspaceQuery.PrepareAsync(session).ConfigureAwait(false);
+
+            var bs = statement.Bind().SetIdempotence(true);
+            await session.ExecuteAsync(bs).ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to drop keyspace.");
+            throw;
+        }
+    }
+
+    class DropKeyspaceQuery : PreparedStatementCache
+    {
+        private const string Template = @"DROP KEYSPACE {0};";
+
+        public DropKeyspaceQuery(ICronusContextAccessor context, ICassandraProvider cassandraProvider) : base(context, cassandraProvider) { }
+
+        internal override string GetQueryTemplate() => Template;
+    }
+}
